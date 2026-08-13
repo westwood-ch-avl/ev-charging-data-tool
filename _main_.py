a9c2be53019acbf84db76843d412c901176df479
Andy <28638927+andypaulphd@users.noreply.github.com>
2026-08-13 16:49:56 -0400

    Misc -- starting to build out various functions in _main_.py

--- _main_.py
+++ _main_.py
@@ -2,6 +2,11 @@ from dotenv import load_dotenv
 import os
 import json
 import requests
+from event import Event
+from ev_charging_user import Ev_Charging_User
+from event_error_checker import isValidEvent
+
+from event_error_checker import isValidEvent
 
 load_dotenv()
 
@@ -14,8 +19,8 @@ def initialize_firebase_service_account():
     cred = credentials.Certificate(key_dict)
     firebase_admin.initialize_app(cred)
 
-##TODO edit this auto-generated code as needed. Need to add params too.
-def get_powerfill_data():
+##TODO edit this auto-generated code as needed. Need to add params too. Also consider baking in a way to use command line to set how far back to pull events.
+def get_powerfill_data() -> list:
     url = os.environ.get("POWERFILL_URL")
     user = os.environ.get("POWERFILL_USER")
     password = os.environ.get("POWERFILL_PASSWORD")
@@ -26,5 +31,43 @@ def get_powerfill_data():
     else:
         raise Exception(f"Failed to fetch data from Powerfill: {response.status_code} - {response.text}")
 
-def handle_powerfill_data(data):
-    ...
+def build_event_list_from_powerfill_list(data) -> list:
+
+    events = []
+
+    for e in data:
+
+        if isValidEvent(e):
+            events.append(Event(e["startValue"], e["stopValue"], e["userId"], e["chargeBoxId"]))
+
+    return events
+
+def get_ev_users_from_db() -> dict:
+
+    import firebase_admin
+    from firebase_admin import firestore
+
+    ev_users = {}
+
+    db = firestore.client()
+    docs = db.collection("ev-charging-users").stream()
+
+    for doc in docs:
+        ev_users[doc.id] = Ev_Charging_User.from_dict(doc.to_dict())
+
+    return ev_users
+
+def post_events_to_db(events: list): #TODO the following is auto generated. Prolly need to fix it. Do these need to be batched? DO I want to store events under each user?
+
+    import firebase_admin
+    from firebase_admin import firestore
+
+    db = firestore.client()
+
+    for event in events:
+        doc_ref = db.collection("events").document(event.generate_doc_key())
+        doc_ref.set(event.to_dict())
+
+def do_monthly_totals():
+
+    ... #TODO
